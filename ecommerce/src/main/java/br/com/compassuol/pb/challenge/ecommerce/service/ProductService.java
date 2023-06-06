c803db47f947fb7b85449e408d00cb0f1a6b71bb
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-06 14:04:16 -0300

    PRODUCT: Implementing method getAll and getById in productService

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/service/ProductService.java
@@ -1,7 +1,11 @@
 package br.com.compassuol.pb.challenge.ecommerce.service;
+import br.com.compassuol.pb.challenge.ecommerce.exception.ProductNotFoundException;
+import br.com.compassuol.pb.challenge.ecommerce.model.Product;
 import org.springframework.stereotype.Service;
 import br.com.compassuol.pb.challenge.ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -11,4 +15,14 @@ public class ProductService {
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + productId));
+    }
+
 }
