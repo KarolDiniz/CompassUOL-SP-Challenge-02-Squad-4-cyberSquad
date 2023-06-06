8229d489370e40538538fcd7c28a4d0fcf158adb
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-06 14:02:23 -0300

    PRODUCT: implementing injection in ProductService

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/service/ProductService.java
@@ -1,6 +1,14 @@
 package br.com.compassuol.pb.challenge.ecommerce.service;
 import org.springframework.stereotype.Service;
+import br.com.compassuol.pb.challenge.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
