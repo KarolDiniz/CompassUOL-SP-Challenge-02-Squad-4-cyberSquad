480c980678e7c675ff5bb6ef90bdcde3278c1612
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-05 21:28:23 -0300

    CUSTOMER: basic modification

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/service/CustomerService.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/service/CustomerService.java
@@ -1,2 +1,29 @@
-package br.com.compassuol.pb.challenge.ecommerce.service;public class CustomerService {
+package br.com.compassuol.pb.challenge.ecommerce.service;
+
+import br.com.compassuol.pb.challenge.ecommerce.model.Customer;
+import br.com.compassuol.pb.challenge.ecommerce.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer getCustomerById(Long customerId) {
+        return customerRepository.findById(customerId)
+                .orElseThrow(() -> new NoSuchElementException("Customer not found"));
+    }
+
+    public Customer updateCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
 }
