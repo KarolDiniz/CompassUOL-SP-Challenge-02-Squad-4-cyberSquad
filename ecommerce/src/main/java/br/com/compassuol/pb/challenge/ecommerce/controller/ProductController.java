7e3f3186040453e65f2ed58f5a453ab0d493b2ed
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-06 14:14:16 -0300

    PRODUCT: Implementing getAll and getById methods in ProductController

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/controller/ProductController.java
@@ -1,4 +1,34 @@
 package br.com.compassuol.pb.challenge.ecommerce.controller;
+import br.com.compassuol.pb.challenge.ecommerce.exception.ProductNotFoundException;
+import br.com.compassuol.pb.challenge.ecommerce.model.Product;
+import br.com.compassuol.pb.challenge.ecommerce.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/products")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Long productId) {
+        try {
+            Product product = productService.getProductById(productId);
+            return ResponseEntity.ok(product);
+        } catch (ProductNotFoundException ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
