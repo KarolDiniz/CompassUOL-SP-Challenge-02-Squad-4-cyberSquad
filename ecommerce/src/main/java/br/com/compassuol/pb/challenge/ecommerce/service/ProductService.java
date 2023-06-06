9dbbccd6637122cec0c41fae6c0fb868b7b4d0ab
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-06 14:08:56 -0300

    PRODUCT: Implementing method updateProduct

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/service/ProductService.java
@@ -1,6 +1,7 @@
 package br.com.compassuol.pb.challenge.ecommerce.service;
 import br.com.compassuol.pb.challenge.ecommerce.exception.ProductNotFoundException;
 import br.com.compassuol.pb.challenge.ecommerce.model.Product;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import br.com.compassuol.pb.challenge.ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,11 @@ public class ProductService {
     public Product getProductById(Long productId) {
         return productRepository.findById(productId)
                 .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + productId));
+    }
+
+    public Product updateProduct(Long productId, Product product) {
+        Product existingProduct = getProductById(productId);
+        BeanUtils.copyProperties(product, existingProduct, "productId");
+        return productRepository.save(existingProduct);
     }
 }
