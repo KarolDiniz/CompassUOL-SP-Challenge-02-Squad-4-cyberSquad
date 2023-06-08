9ad6f07ec389b566c9744164860f1e044d9fc2cf
Bianca Balabenute <balabenute.bianca@gmail.com>
2023-06-08 12:34:37 -0300

    ORDER: create order

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/model/Order.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/model/Order.java
@@ -1,4 +1,33 @@
 package br.com.compassuol.pb.challenge.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+
+    @NotNull
+    @Column(name = "date")
+    private LocalDateTime date;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    @Column(name = "status")
+    private OrderStatus status;
 }
