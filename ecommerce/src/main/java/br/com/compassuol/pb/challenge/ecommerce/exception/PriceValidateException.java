c60de6af649bb0f6023b1c0690d4ac93061f8586
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-08 20:38:15 -0300

    REFATORING: implementing PriceValidateException

---
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/exception/PriceValidateException.java
@@ -0,0 +1,6 @@
+package br.com.compassuol.pb.challenge.ecommerce.exception;
+public class PriceValidateException extends RuntimeException {
+    public PriceValidateException(String message) {
+        super(message);
+    }
+}
