a4bd67980153b080c2c7b6f5d12b430faf66a2d7
Bianca Balabenute <balabenute.bianca@gmail.com>
2023-06-09 14:38:52 -0300

    TEST/DEVELOPMENT/CUSTOMER: performing customer tests

---
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/model/CustomerTest.java
@@ -0,0 +1,102 @@
+import br.com.compassuol.pb.challenge.ecommerce.model.Customer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @Test
+    void getCustomerId() {
+        Customer customer = new Customer();
+        customer.setCustomerId(1L);
+
+        assertEquals(1L, customer.getCustomerId());
+    }
+
+    @Test
+    void getName() {
+        Customer customer = new Customer();
+        customer.setName("John");
+
+        assertEquals("John", customer.getName());
+    }
+
+    @Test
+    void getCpf() {
+        Customer customer = new Customer();
+        customer.setCpf("12345678900");
+
+        assertEquals("12345678900", customer.getCpf());
+    }
+
+    @Test
+    void getEmail() {
+        Customer customer = new Customer();
+        customer.setEmail("john@example.com");
+
+        assertEquals("john@example.com", customer.getEmail());
+    }
+
+    @Test
+    void isActive() {
+        Customer customer = new Customer();
+        customer.setActive(true);
+
+        assertTrue(customer.isActive());
+    }
+
+    @Test
+    void setCustomerId() {
+        Customer customer = new Customer();
+        customer.setCustomerId(1L);
+
+        assertEquals(1L, customer.getCustomerId());
+    }
+
+    @Test
+    void setName() {
+        Customer customer = new Customer();
+        customer.setName("John");
+
+        assertEquals("John", customer.getName());
+    }
+
+    @Test
+    void setCpf() {
+        Customer customer = new Customer();
+        customer.setCpf("12345678900");
+
+        assertEquals("12345678900", customer.getCpf());
+    }
+
+    @Test
+    void setEmail() {
+        Customer customer = new Customer();
+        customer.setEmail("john@example.com");
+
+        assertEquals("john@example.com", customer.getEmail());
+    }
+
+    @Test
+    void setActive() {
+        Customer customer = new Customer();
+        customer.setActive(true);
+
+        assertTrue(customer.isActive());
+    }
+
+    @Test
+    void testEquals() {
+        Customer customer1 = new Customer();
+        customer1.setCustomerId(1L);
+
+        Customer customer2 = new Customer();
+        customer2.setCustomerId(1L);
+
+        Customer customer3 = new Customer();
+        customer3.setCustomerId(2L);
+
+        assertEquals(customer1, customer2);
+        assertNotEquals(customer1, customer3);
+    }
+}
