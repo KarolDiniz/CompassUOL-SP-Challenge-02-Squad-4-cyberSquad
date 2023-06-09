5ebe6219d33568b01484f2b252c76ae10df7e1ec
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-09 20:13:39 -0300

    ORDER: Implementing attributes

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/model/Order.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/model/Order.java
@@ -1,4 +1,35 @@
 package br.com.compassuol.pb.challenge.ecommerce.model;
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.List;
+@Data
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(name = "customer_id", nullable = false)
+    private Long customerId;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> items;
+
+    // Construtores, getters e setters
+
+    @PrePersist
+    public void prePersist() {
+        this.date = LocalDateTime.now();
+    }
 }
