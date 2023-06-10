e4afa65291f469311ed7b63b2de8e04aeae6d3da
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-10 16:03:50 -0300

    PAYMENT: implementing attributes

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/domain/entities/Payment.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/domain/entities/Payment.java
@@ -1,4 +1,35 @@
 package br.com.compassuol.pb.challenge.ecommerce.domain.entities;
 
+import br.com.compassuol.pb.challenge.ecommerce.domain.enums.PaymentMethod;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "payment")
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "orderId", nullable = false)
+    @Positive
+    private Long orderId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "paymentMethod", nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "payment_date", nullable = false)
+    private LocalDateTime paymentDate;
+
+    @PrePersist
+    public void prePersist() {
+        this.paymentDate = LocalDateTime.now();
+    }
 }
