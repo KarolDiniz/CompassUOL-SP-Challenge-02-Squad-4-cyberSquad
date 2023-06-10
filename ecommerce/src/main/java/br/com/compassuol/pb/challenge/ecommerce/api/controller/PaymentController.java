1a172e953a78495d5891836dbbf54916acea727d
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-10 16:07:25 -0300

    PAYMENT: implementing controller

---
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/api/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package br.com.compassuol.pb.challenge.ecommerce.api.controller;
+import br.com.compassuol.pb.challenge.ecommerce.domain.entities.Payment;
+import br.com.compassuol.pb.challenge.ecommerce.domain.service.PaymentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/payments")
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Payment> createPayment(@Valid @RequestBody Payment payment) {
+        try {
+            Payment createdPayment = paymentService.createPayment(payment);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdPayment);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
+
